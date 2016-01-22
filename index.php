d867c9df37f218e84d2e5410569d48fac4a08af4
pacster <martinez@raidho.mx>
2016-01-22 12:43:29 -0600

    Markup y Estilos base + GITIGNORE
    - Se agrega INDEX basado en HTML5 Boilerplate
    - Se genera una hoja de estilos RESETS, basada en la propuesta de http://jaydenseric.com/blog/forget-normalize-or-resets-lay-your-own-css-foundation
    - Se generan hojas iniciales de SASS, usando estilos propuestos en SKELETON (se limpió la hoja original de SKELETON y se tradujo a SASS para incluírla como template por lo pronto)
    
    - Se agrega un GITIGNORE para excluír cahce de SASS y configuraciones de Codekit
    
    Configuración inicial lista para comenzar a establecer layouts fluidos y comenzar a personalizar colores, tipografía y tamaños

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+/*
+Designaholic Templates
+Index
+*/
+?>
+
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>Designaholic</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+	<link rel="icon" type="image/png" href="favicon.png">
+
+	<link rel="stylesheet" href="css/resets.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<!--[if lt IE 8]>
+	<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+
+	<div class="container">
+		<div class="row">
+			<div class="one-half column">
+				<h4>Primer template</h4>
+				<p>Base para el desarrollo de las maquetas para <a href="http://designaholic.mx/">Designaholic.mx</a>.</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- Scripts -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+
+	<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+	<script>
+	(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+		function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+		e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+		e.src='https://www.google-analytics.com/analytics.js';
+		r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+		ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+	</script>
+</body>
+</html>
