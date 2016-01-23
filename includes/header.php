dbbfc2914aa91fcf09ad97e238d9ec0ff59c61b4
pacster <martinez@raidho.mx>
2016-01-23 16:30:27 -0600

    Se añade estructura básica para el homepage

---
+++ includes/header.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Designaholic Templates
+Header
+*/
+?>
+
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>Designaholic</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+	<link rel="icon" type="image/png" href="favicon.png">
+
+	<link rel="stylesheet" href="css/resets.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<!--[if lt IE 8]>
+	<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+
+	<header>
+		<nav id="top_menu">
+			<div class="container">
+			<ul>
+				<li><a href="#">Reciente</a></li><li>
+					<a href="#">Acerca</a></li><li>
+					<a href="#">Columnas</a></li><li>
+					<a href="#">Categorías</a></li><li>
+					<a href="#">Archivo</a></li><li>
+					<a href="#">Autores</a></li><li>
+					<a href="#">Buscar</a></li>
+			</ul>
+			</div>
+		</nav>
+		<div id="top_menu_categories">
+			<div class="container">
+			<ul>
+				<li><a href="#">Arquitectura</a></li><li>
+					<a href="#">Comercial</a></li><li>
+					<a href="#">Interiores</a></li><li>
+					<a href="#">Gráfico</a></li><li>
+					<a href="#">Gadgets</a></li><li>
+					<a href="#">Moda</a></li><li>
+					<a href="#">Producto</a></li><li>
+					<a href="#">Mobiliario</a></li>
+			</ul>
+			</div>
+		</div>
+	</header>
--- index.php
+++ index.php
@@ -5,64 +5,170 @@ Index
 */
 ?>
 
-<!doctype html>
-<html class="no-js" lang="">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title>Designaholic</title>
-	<meta name="description" content="">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php include 'includes/header.php' ?>
 
-	<link rel="apple-touch-icon" href="apple-touch-icon.png">
-	<link rel="icon" type="image/png" href="favicon.png">
+	<div class="container spacer">
 
-	<link rel="stylesheet" href="css/resets.css">
-	<link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-	<!--[if lt IE 8]>
-	<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-	<![endif]-->
-
-	<header>
-		<nav id="top_menu">
-			<div class="container">
-			<ul>
-				<li><a href="#">Reciente</a></li><li>
-					<a href="#">Acerca</a></li><li>
-					<a href="#">Columnas</a></li><li>
-					<a href="#">Categorías</a></li><li>
-					<a href="#">Archivo</a></li><li>
-					<a href="#">Autores</a></li><li>
-					<a href="#">Buscar</a></li>
-			</ul>
+		<!-- Primer bloque de 3 destacados -->
+		<div class="home_destacados_uno">
+			<h2>#AbiertoDeDiseño2015</h2>
+			<div class="row">
+				<div class="three columns">
+					<a href="#">
+						<img src="img/placeholders/row_img1.jpg" alt="">
+						<div class="meta">
+							<span>Gadgets</span>
+							<span>08/12/15</span>
+						</div>
+						<h3>Los Nuevos Chromecasts por Google</h3>
+					</a>
+				</div>
+				<div class="six columns">
+					<a href="#">
+						<img src="img/placeholders/row_img2.jpg" alt="">
+						<div class="meta">
+							<span>Arquitectura</span>
+							<span>08/12/15</span>
+						</div>
+						<h2>Casey Gerald: La Importancia de un Propósito</h2>
+					</a>
+				</div>
+				<div class="three columns">
+					<a href="#">
+						<img src="img/placeholders/row_img3.jpg" alt="">
+						<div class="meta">
+							<span>Urbanismo</span>
+							<span>08/12/15</span>
+						</div>
+						<h3>Heidi Grant Halvorson: ¿Por qué Nadie te Quiere?</h3>
+					</a>
+				</div>
 			</div>
-		</nav>
-		<div id="top_menu_categories">
-			<div class="container">
-			<ul>
-				<li><a href="#">Arquitectura</a></li><li>
-					<a href="#">Comercial</a></li><li>
-					<a href="#">Interiores</a></li><li>
-					<a href="#">Gráfico</a></li><li>
-					<a href="#">Gadgets</a></li><li>
-					<a href="#">Moda</a></li><li>
-					<a href="#">Producto</a></li><li>
-					<a href="#">Mobiliario</a></li>
-			</ul>
+
+			<!-- Primer bloque de Ads -->
+			<div class="row home_ads_uno">
+				<div class="ten columns offset-by-one">
+					<div class="ad_dh_uno">
+						<img src="img/placeholders/ad_dh1.jpg" alt="">
+						<h4>Nuevos espacios para Baker & McKenzie, por IHO Espacios / Knoll.</h4>
+						<span><a>Publicidad de nuestros patrocinadores.
+						<br><a href="#">Anúnicate aquí
+						</a>.</span>
+					</div>
+					<div class="ad_google_uno">
+						<img src="img/placeholders/ad_google1.jpg" alt="">
+					</div>
+				</div>
 			</div>
 		</div>
-	</header>
 
-	<div class="container">
-		<div class="row">
-			<div class="one-half column">
-				<h4>Primer template</h4>
-				<p>Base para el desarrollo de las maquetas para <a href="http://designaholic.mx/">Designaholic.mx</a>.</p>
+	</div><!-- Container -->
+
+	<!-- Columnas Designaholic -->
+
+	<div class="container spacer">
+
+		<!-- Artículos Recientes -->
+		<div class="home_destacados_dos">
+			<h2>Artículos Recientes</h2>
+
+			<div class="row">
+				<div class="six columns">
+					<a href="#">
+						<img src="img/placeholders/row_img1.jpg" alt="">
+						<div class="meta">
+							<span>Categoría</span>
+							<span>08/12/15</span>
+						</div>
+						<h2>El SUV Model X de Tesla</h2>
+					</a>
+				</div>
+				<div class="six columns">
+					<a href="#">
+						<img src="img/placeholders/row_img2.jpg" alt="">
+						<div class="meta">
+							<span>Categoría</span>
+							<span>08/12/15</span>
+						</div>
+						<h2>Tylko e Yves Behar Desarrollan Productos Personalizables</h2>
+					</a>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="three columns galeria">
+					<a href="#">
+						<img src="img/placeholders/row_img3.jpg" alt="">
+						<div class="meta">
+							<span>Categoría</span>
+							<span>08/12/15</span>
+						</div>
+						<h4><span class="rojo_txt">Galería:</span> Ettore Sottsasss Antes de Memphis</h4>
+					</a>
+				</div>
+				<div class="three columns galeria">
+					<a href="#">
+						<img src="img/placeholders/row_img4.jpg" alt="">
+						<div class="meta">
+							<span>Categoría</span>
+							<span>08/12/15</span>
+						</div>
+						<h4>Abierto Mexicano de Disenno 2015: La Revista Fragmentada, Introspectiva editorial de Sada por Casa Gutiérez Nájera</h4>
+					</a>
+				</div>
+				<div class="three columns galeria">
+					<a href="#">
+						<img src="img/placeholders/row_img5.jpg" alt="">
+						<div class="meta">
+							<span>Categoría</span>
+							<span>08/12/15</span>
+						</div>
+						<h4>MP01: Tecnología Domesticada por Punkt</h4>
+					</a>
+				</div>
+				<div class="three columns galeria">
+					<a href="#">
+						<img src="img/placeholders/row_img6.jpg" alt="">
+						<div class="meta">
+							<span>Categoría</span>
+							<span>08/12/15</span>
+						</div>
+						<h4><span class="rojo_txt">Video:</span> Kent Larson: Diseños Inteligentes para Acomodar más Personas en cada Ciudad</h4>
+					</a>
+				</div>
 			</div>
 		</div>
-	</div>
+
+		<div class="ad_google_dos">
+			<img src="img/placeholders/ad_google2.jpg" alt="">
+		</div>
+
+		<!-- Festival Decode -->
+		<div class="home_destacados_tres">
+			<h2>Festival Decode</h2>
+			<div class="row">
+				<div class="three columns">
+					<a href="#">
+						<img src="img/placeholders/row_img1.jpg" alt="">
+						<h3>Decode celebra el diseño y sus diferentes vertientes en un fin de semana con actividades relacionadas con el diseño, la creatividad y la innovación.</h3>
+					</a>
+				</div>
+				<div class="six columns">
+					<a href="#">
+						<img src="img/placeholders/row_img2.jpg" alt="">
+						<h2>Resumen del Festival Decode 2015</h2>
+					</a>
+				</div>
+				<div class="three columns">
+					<a href="#">
+						<img src="img/placeholders/row_img3.jpg" alt="">
+						<h3>Exposición Generación Decode 2015</h3>
+					</a>
+				</div>
+			</div>
+		</div>
+
+	</div><!-- Container -->
 
 	<!-- Scripts -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
