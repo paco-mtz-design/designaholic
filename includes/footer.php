5a4758c38ebdc74d520808f997a978d5be48e8fa
pacster <martinez@raidho.mx>
2016-01-25 16:55:56 -0600

    Se mueve el footer a su propio template

---
+++ includes/footer.php
@@ -0,0 +1,60 @@
+<?php
+/*
+Designaholic Templates
+Footer
+*/
+?>
+
+<!-- Footer -->
+<div id="footer" class="fondo_azul_oscuro bloque_horizontal">
+	<div class="container spacer">
+		<h2>Desde 2009, Designaholic.mx difunde el trabajo de diseñadores mexicanos e internacionales, promueve crítica y opinión sobre varias disciplinas de Diseño, y cubre los eventos de Diseño más importantes a nivel internacional.</h2>
+		<div class="row">
+			<div class="four columns">
+				<h4>Suscríbete a nuestro newsletter</h4>
+				<form action="" class="forma_negativo">
+					<input type="text" placeholder="Tu Nombre">
+					<input type="text" placeholder="Tu Correo">
+					<input type="submit" value="Suscríbete">
+				</form>
+			</div>
+			<div class="four columns">
+				<h4>Síguenos en redes</h4>
+				<ul class="follow_links">
+					<li class="follow_twitter"><a href="#">Tw</a></li><li class="follow_google">
+						<a href="#">G+</a></li><li class="follow_facebook">
+						<a href="#">Fb</a></li><li class="follow_linkedin">
+						<a href="#">Li</a></li><li class="follow_rss">
+						<a href="#">Rss</a></li><li class="follow_pinterest">
+						<a href="#">Pi</a>
+					</li>
+				</ul>
+			</div>
+			<div class="three columns offset-by-one">
+				<h4>Ligas de interés</h4>
+				<ul>
+					<li><a href="#">Contacto</a></li>
+					<li><a href="#">Patrocinadores</a></li>
+					<li><a href="#">Aviso de Privacidad</a></li>
+					<li><a href="#">Mapa de Sitio</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Scripts -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+	function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+	e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+	e.src='https://www.google-analytics.com/analytics.js';
+	r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+	ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+</script>
+</body>
+</html>
