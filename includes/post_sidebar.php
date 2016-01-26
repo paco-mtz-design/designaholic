fe3d39b22c4b02f93846c506ee78b4d021f2441a
pacster <martinez@raidho.mx>
2016-01-26 12:52:21 -0600

    Template Single Post 95%

--- includes/post_sidebar.php
+++ includes/post_sidebar.php
@@ -1,12 +1,24 @@
 <?php
 /*
 Designaholic Templates
-Loop 4 artículos
+Single Post Sidebar
 */
 ?>
 
-<div class="row">
-	<div class="four columns articulo galeria">
+<!-- Sidebar Single Post -->
+<div class="sidebar_post three columns offset-by-one">
+
+	<h3>Publicidad</h3>
+
+	<div class="ad_group">
+		<div class="ad_google_uno">
+			<img src="img/placeholders/ad_google1.jpg" width="300" height="250" alt="">
+		</div>
+	</div>
+
+	<h3>Reciente</h3>
+
+	<div class="articulo galeria">
 		<a href="single_post.php">
 			<img src="img/placeholders/row_img1.jpg" alt="">
 			<div class="meta">
@@ -16,7 +28,7 @@ Loop 4 artículos
 			<h4><span class="rojo_txt">Galería:</span> Ettore Sottsasss Antes de Memphis</h4>
 		</a>
 	</div>
-	<div class="four columns articulo">
+	<div class="articulo">
 		<a href="#">
 			<img src="img/placeholders/row_img2.jpg" alt="">
 			<div class="meta">
@@ -26,7 +38,7 @@ Loop 4 artículos
 			<h4>Abierto Mexicano de Disenno 2015: La Revista Fragmentada, Introspectiva editorial de Sada por Casa Gutiérez Nájera</h4>
 		</a>
 	</div>
-	<div class="four columns articulo">
+	<div class="articulo">
 		<a href="#">
 			<img src="img/placeholders/row_img3.jpg" alt="">
 			<div class="meta">
@@ -36,4 +48,15 @@ Loop 4 artículos
 			<h4>MP01: Tecnología Domesticada por Punkt</h4>
 		</a>
 	</div>
+	<div class="articulo video">
+		<a href="#">
+			<img src="img/placeholders/row_img6.jpg" alt="">
+			<div class="meta">
+				<span>Categoría</span>
+				<span>08/12/15</span>
+			</div>
+			<h4><span class="rojo_txt">Video:</span> Kent Larson: Diseños Inteligentes para Acomodar más Personas en cada Ciudad</h4>
+		</a>
+	</div>
+
 </div>
