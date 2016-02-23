87d2cbb4ad070bf299aacf06ab0ce5cd2ba580f2
Linda Castillor <linda@raidho.mx>
2016-02-22 19:17:20 -0600

    Mover script a footer de menu

--- includes/footer.php
+++ includes/footer.php
@@ -84,6 +84,46 @@ var n = $( ".img_cont" ).length;
 $( ".counter span" ).append( $( "<b></b>" ) );
 $( ".counter span b" ).text( "de " + n );
 
+// Toggle Menu
+$("#burger").click(function(){
+	$("header").toggleClass("show_menu");
+});
+
+// Toggle Search Box
+$("#search").click(function(){
+	$(".searchbox").toggleClass("show_search");
+});
+
+$(function(){
+	$('#list').children().wrapAll('<div class="wrapper"></div>');
+	var expandedHeight = $('.wrapper').height();
+	var collapsedHeight = $('#list').height();
+	//$('p').text(innerHeight);
+	$('#burger').on("click", function(){
+		if ($(this).hasClass("expand")) {
+			$("#list").animate({
+				height: 0
+			}, 200, function(){
+				$('.expand').toggleClass("expand reset");
+			});
+		}
+		else {
+			$("#list").animate({height: expandedHeight}, 200 );
+			$(this).toggleClass("expand reset");
+		}
+	});
+});
+
+function checkWidth() {
+	if ($(window).width() < 750) {
+		$('#list').removeClass('hide_list');
+	} else {
+		$('#list').addClass('hide_list');
+	}
+}
+$(window).resize(checkWidth);
+
+// http://stackoverflow.com/questions/2758651/how-to-change-height-div-on-window-resize
 </script>
 
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
