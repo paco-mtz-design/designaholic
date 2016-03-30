a14c75a5886fbaaaad2296af8525accb83ba2d0a
Linda Castillor <linda@raidho.mx>
2016-03-30 15:49:33 -0600

    Ajustar script de menu

--- includes/footer.php
+++ includes/footer.php
@@ -61,6 +61,8 @@ Footer
 <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js"></script>
 
 <script type="text/javascript">
+
+// Slick Slider
 $('.slider-for').slick({
 	slidesToShow: 1,
 	slidesToScroll: 1,
@@ -68,6 +70,7 @@ $('.slider-for').slick({
 	fade: true,
 	asNavFor: '.slider-nav',
 });
+
 
 $('.slider-nav').slick({
 	variableWidth: true,
@@ -80,43 +83,37 @@ $('.slider-nav').slick({
 	focusOnSelect: true
 });
 
+// Slick Image Counter
 var n = $( ".img_cont" ).length;
 $( ".counter span" ).append( $( "<b></b>" ) );
 $( ".counter span b" ).text( "de " + n );
 
-// Toggle Menu
-$("#burger").click(function(){
-	$("header").toggleClass("show_menu");
-});
-
 // Toggle Search Box
-$("#search").click(function(){
-	$(".searchbox").toggleClass("show_search");
+$( "#search" ).click(function(){
+	$( ".searchbox" ).toggleClass( "show_search" );
 });
 
+// Trigger Menu
+$(function() {
+	var numberItems = $( "#list li" ).length;
+	var heightItems = $( "#list li" ).outerHeight();
+	var totalSize = numberItems * heightItems
 
-$(function(){
-	$('#list').children().wrapAll('<div class="wrapper"></div>');
-	var expandedHeight = $('.wrapper').height();
-	var collapsedHeight = $('#list').height();
-	//$('p').text(innerHeight);
-	$('#burger').on("click", function(){
-		if ($(this).hasClass("expand")) {
-			$("#list").animate({
-				height: 0
-			}, 200, function(){
-				$('.expand').toggleClass("expand reset");
-			});
+	$( "#list" ).children().wrapAll('<div class="wrapper"></div>');
+	$( "#burger" ).on("click", function(){
+		$("header").toggleClass( "show_menu" );
+		if ($(this).hasClass( "expand" )) {
+			$( "#list" ).animate({ height: "0px" }, 200);
+			$(this).toggleClass( "expand reset" );
 		}
 		else {
-			$("#list").animate({height: expandedHeight}, 200 );
-			$(this).toggleClass("expand reset");
+			$( "#list" ).animate({ height: totalSize }, 200 );
+			$(this).toggleClass( "expand reset" );
 		}
 	});
-});
+})
 
-
-
+// If menu is open: Hide height above 750px
 function checkWidth() {
 	if ($(window).width() < 750) {
 		$('#list').removeClass('hide_list');
@@ -126,7 +123,6 @@ function checkWidth() {
 }
 $(window).resize(checkWidth);
 
-// http://stackoverflow.com/questions/2758651/how-to-change-height-div-on-window-resize
 </script>
 
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
