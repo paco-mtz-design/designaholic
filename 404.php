47bed4b2d3678a249be6240065bb173e3b8f1f5e
Linda Castillor <linda@raidho.mx>
2016-03-31 12:40:34 -0600

    Markup para 404.php

---
+++ 404.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Designaholic Templates
+Categoría
+*/
+?>
+
+<?php include 'includes/header.php' ?>
+
+	<!-- Portada de categoría -->
+	<div class="bloque_horizontal cat_cover" style="background-image: url('https://media.giphy.com/media/lCkumTggV53xe/giphy.gif');">
+		<div class="container">
+			<h1>Este contenido no esta disponible.</h1>
+		</div>
+	</div>
+
+	<div class="container spacer">
+		<h2>Artículos que te podrían interesar.</h2>
+	</div>
+
+	<div class="container spacer">
+
+		<?php include 'includes/loop4col.php' ?>
+
+		<?php include 'includes/ad_dh_dos.php' ?>
+
+	</div>
+
+<?php include 'includes/footer.php' ?>
